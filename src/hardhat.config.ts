edce9ada8b8d3c95b9cef628a6c382c60c99323f
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-08-05 21:31:09 +0200

    Setup project skeleton

---
+++ src/hardhat.config.ts
@@ -0,0 +1,14 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.21",
+      },
+    ],
+  },
+};
+
+export default config;
