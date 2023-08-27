ce2252d0279f553873eb96633e7f2c1f50bad0aa
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-08-27 15:48:19 +0200

    Implement deployment script

--- src/hardhat.config.ts
+++ src/hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const { OPT_MAINNET_ALCHEMY_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -8,6 +11,15 @@ const config: HardhatUserConfig = {
         version: "0.8.21",
       },
     ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 10,
+      forking: {
+        enabled: false, //set to 'false' when API KEY env var is not set
+        url: `https://opt-mainnet.g.alchemy.com/v2/${OPT_MAINNET_ALCHEMY_API_KEY}`,
+      },
+    },
   },
 };
 
