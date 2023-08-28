d15650885d5c92a80a444ef5097bd503e74280c5
xRave110 <xRave110@protonmail.com>
2023-08-28 09:32:44 +0200

    Pseudocode for main interfaces

--- src/contracts/CL_Executor.sol
+++ src/contracts/CL_Executor.sol
@@ -4,9 +4,18 @@ pragma solidity >=0.8.0;
 
 import "../interfaces/IRamsesV2Pool.sol";
 import "../interfaces/ISwapRouter.sol";
+import "../interfaces/INonfungiblePositionManager.sol";
 
 contract CL_Compounder {
+    /* Temporary */
+    address constant NFT_MANAGER_ADDRESS =
+        0xAA277CB7914b7e5514946Da92cb9De332Ce610EF;
+
     ISwapRouter immutable swapRouter;
+    INonfungiblePositionManager nonfungiblePositionManager =
+        INonfungiblePositionManager(NFT_MANAGER_ADDRESS);
+
+    mapping(address => uint256) positionTokenToNftId;
 
     constructor(address routerAddress) {
         swapRouter = ISwapRouter(routerAddress);
@@ -22,9 +31,22 @@ contract CL_Compounder {
         address tokenB,
         uint256 amountA,
         uint256 amountB,
-        uint256 fee
+        uint256 fee,
+        int24 tickLower,
+        int24 tickUpper
     ) public returns (uint256) {
         IRamsesV2Pool currentPool = swapRouter.getPool(tokenA, tokenB, fee);
+
+        MintParams params = MintParams(
+            tokenA,
+            tokenB,
+            fee,
+            amountA,
+            amountB,
+            tickLower,
+            tickUpper
+        );
+        nonfungiblePositionManager.mint(params);
         // swapRouter.mint(address(this), )
     }
 
@@ -37,17 +59,39 @@ contract CL_Compounder {
         address tokenIn,
         address tokenOut,
         uint256 amountMin
-    ) public returns (uint256) {}
+    ) public returns (uint256) {
+        bytes path = abi.encode(tokenIn, tokenOut);
+        ExactOutputParams params = ExactOutputParams(
+            path,
+            msg.sender,
+            (block.timestamp + 10),
+            amountMin,
+            IERC20(tokenIn).balanceOf(msg.sender)
+        );
+        swapRouter.exactOutput(params);
+    }
 
     /**
     @dev collect gathered fees, collect gathered RAM token, provide collected fees into the pool, boost rewards with RAM token
     */
-    function compoundPosition(address positionToken) public returns (uint256) {}
+    function compoundPosition(address positionToken) public returns (uint256) {
+        _collectRewards(positionToken);
+        provideLiquidity();
+        _boostRewards(positionToken);
+    }
 
     /** Private setters **/
     function _boostRewards(address positionToken) private returns (uint256) {}
 
-    function _compoundFees(address positionToken) private returns (uint256) {}
+    function _collectRewards(address positionToken) private returns (uint256) {
+        bytes params = CollectParams(
+            positionTokenToNftId[positionToken],
+            address(this),
+            0 /* ?? */,
+            0 /* ?? */
+        );
+        nonfungiblePositionManager.collect(params);
+    }
 
     /** Public getters **/
     function calculateTriggerToCompound(
